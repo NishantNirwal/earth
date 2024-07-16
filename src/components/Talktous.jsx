0c61a50ba5bbed4556c4d39ae0f6828a244edd01
NishantNirwal <166308303+NishantNirwal@users.noreply.github.com>
2024-07-16 17:18:06 +0530

    work on talktous

--- src/components/Talktous.jsx
+++ src/components/Talktous.jsx
@@ -3,8 +3,8 @@ import './styles/Talktous.css'
 
 function Talktous() {
   return (
-    <section className="ques">
-    <div className="ques-div">
+    <section className="talkto">
+    <div className="talkto-div">
         <h2>TALK TO US</h2>
         <p>Have any questions? We are always open to talk about your business,<br/> new projects, creative opportunities and how we can help you.</p>
         <button className="main-btn">GET IN TOUCH</button>
